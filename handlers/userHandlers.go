e5c4e1e777112fbce8e0c1275aae7280e4c861ae
denisunderonov <denisunderonov2@gmail.com>
2026-01-25 12:20:01 +0300

    Добавил поиск пользователя по id

--- handlers/userHandlers.go
+++ handlers/userHandlers.go
@@ -1,11 +1,65 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"authgo/data"
+	"authgo/models"
+	"strconv"
+)
 
-func createHandler(w http.ResponseWriter, r *http.Request) {
+func CreateUser(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		http.Error(w, "Данный метод не поддерживается", http.StatusMethodNotAllowed)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
+	var input struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	user := &models.User{
+		ID:       data.NextUserID,
+		Username: input.Username,
+		Password: input.Password,
+	}
+
+	data.Users[user.ID] = user
+	data.NextUserID++
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
 }
+
+func getUserByID(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+		return
+	}
+	idStr := r.URL.Query().Get("id")
+	if idStr == "" {
+		http.Error(w, "ID пользователя не указан", http.StatusBadRequest)
+		return
+	}
+	
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Некорректный ID пользователя", http.StatusBadRequest)
+		return
+	}
+
+	user, exists := data.Users[id]
+	if !exists {
+		http.Error(w, "Пользователь не найден", http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
+	
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"authgo/handlers"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/create", handlers.CreateUser)
+
+	fmt.Println("Сервер запущен на порту 8080")
+	http.ListenAndServe(":8080", nil)
 }
