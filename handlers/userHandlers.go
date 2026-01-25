be42e9ee3d7e4db656efbfaaa932dd0c9a91ddff
denisunderonov <denisunderonov2@gmail.com>
2026-01-25 12:45:36 +0300

    Добавил удаление пользователей по ID и получение всего массива пользователей

--- handlers/userHandlers.go
+++ handlers/userHandlers.go
@@ -37,7 +37,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(user)
 }
 
-func getUserByID(w http.ResponseWriter, r *http.Request) {
+func GetUserByID(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
 		return
@@ -61,5 +61,47 @@ func getUserByID(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
+}
+
+func DeleteUserByID(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+		return
+	}
+	idStr := r.URL.Query().Get("id")
+	if idStr == "" {
+		http.Error(w, "ID пользователя не указан", http.StatusBadRequest)
+		return
+	}
+	
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Некорректный ID пользователя", http.StatusBadRequest)
+		return
+	}
+
+	_, exists := data.Users[id]
+	if !exists {
+		http.Error(w, "Пользователь не найден", http.StatusNotFound)
+		return
+	}
+
+	delete(data.Users, id)
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+		return
+	}
+	
+	users := make([]*models.User, 0, len(data.Users))
+	for _, user := range data.Users {
+		users = append(users, user)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
 }
 	
